dfb42b5191c899c1f8b282ed3766bdeb05e8ae1b
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-14 14:27:15 +0000

    Parse the CSV file

---
+++ SupportBank.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SupportBank.ConsoleApp
+{
+  class Program
+  {
+    static void Main()
+    {
+      var parsedData = ReadCSV(@"Transactions2014.csv");
+
+      foreach (var transaction in parsedData)
+      {
+        Console.WriteLine($"{transaction.Date:d}: {transaction.From} paid {transaction.To} {transaction.Amount:C} for {transaction.Narrative}");
+      }
+
+      Console.ReadLine();
+    }
+
+    private static IEnumerable<Transaction> ReadCSV(string filename)
+    {
+      var lines = File.ReadAllLines(filename).Skip(1);
+
+      foreach (var line in lines)
+      {
+        var fields = line.Split(',');
+
+        yield return new Transaction
+        {
+          Date = DateTime.Parse(fields[0]),
+          From = fields[1],
+          To = fields[2],
+          Narrative = fields[3],
+          Amount = decimal.Parse(fields[4])
+        };
+      }
+    }
+  }
+}
---
+++ SupportBank.ConsoleApp/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SupportBank.ConsoleApp
+{
+  internal class Transaction
+  {
+    public DateTime Date { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public string Narrative { get; set; }
+    public decimal Amount { get; set; }
+  }
+}
